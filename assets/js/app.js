8dede72f8dba2c12c0e7a6a0a52c8d7b4c4c90f5
flipmotion <flipmotions@gmail.com>
2016-03-11 00:50:33 +0300

    UP
    map
    header fix
    form

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,7 @@ $(document).ready(function () {
 	window.addEventListener("scroll", scrolling);
 	$(window).resize(function () {
 		scrolling();
+		// $('.header-lvl-2').css({'top':'0px'});
 	});
 	$(".owl-carousel").owlCarousel({
 		items: 3,
@@ -73,22 +74,18 @@ $(document).ready(function () {
 			zoom: 17,
 			controls: []
 		}), myMap.behaviors.disable(['rightMouseButtonMagnifier', 'scrollZoom']);
-		myPlacemark = new ymaps.Placemark([55.784380, 37.636997], {
-			hintContent: [''].join(''),
+		var myPlacemark = new ymaps.Placemark([55.7844, 37.636697], {
+			hintContent: ['1'].join(''),
 			balloonContentBody: ['' // '<div class=\'map_holder\'><div class=\'map_header\'><p>Контакты</p><\/div><div class=\'map_address\'><div class=\'icon\'><\/div><p>г. Москва, м. Митино</p><\/div><div class=\'map_phone\'><div class=\'icon\'><\/div><p><strong>+ 7 965-242-97-42</strong></p><p><strong>+ 7 926-113-58-17</strong></p><\/div><div class=\'map_date\'><div class=\'icon\'><\/div><p>Пн-Пт с 09:00 до 18:00</p><\/div><div class=\'map_mail\'><div class=\'icon\'><\/div><p><a href="mailto:Topsales15@mail.ru">Topsales15@mail.ru</a></p><\/div><\/div>'
 			].join('')
 		}, {
 			iconLayout: 'default#image',
-			iconImageHref: '',
-			iconImageSize: [20, 20],
-			iconImageOffset: [-23, -70]
+			iconImageHref: 'assets/img/address.png',
+			iconImageSize: [13, 17],
+			iconImageOffset: [0, 0]
 		});
 		myMap.geoObjects.add(myPlacemark);
 	}
-	var map = new ymaps.Map("map", {
-		center: [55.76, 37.64],
-		zoom: 7
-	});
 	scrolling();
 });
 
@@ -104,15 +101,15 @@ function scrolling() {
 
 	var scrolled = document.documentElement.scrollTop || window.pageYOffset,
 	    header = document.querySelector('.header-lvl-2'),
+	    headerL = document.querySelector('.header'),
 	    headerHeight = header.clientHeight;
-	header.style.marginTop = 0 + 'px';
 	if (0 < scrolled < headerHeight) {
 		var j = scrolled;
 		if (j > headerHeight) {
 			j = headerHeight;
 		}
-		header.style.marginTop = -j + 'px';
+		header.style.top = -j + 'px';
 	} else {
-		header.style.marginTop = 0 + 'px';
+		header.style.top = 0 + 'px';
 	}
 }
